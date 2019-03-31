64b7155321fb3ccee380e1ab450ed8dcf3a180b5
muhammad. nur ichsan <49121655+1829140014@users.noreply.github.com>
2019-03-31 20:59:53 +0700

    Create uts1ichsan.cpp

---
+++ uts1ichsan.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  cout<< " nama    : muhammad. nur ichsan "<<endl;
+  cout<< " nim     : 1829140014 "<<endl;
+  cout<< " kelas   : teknik komputer b "<<endl;
+  cout<< " angkatan: 2018 "<<endl;
+  return 0;
+  
+}
